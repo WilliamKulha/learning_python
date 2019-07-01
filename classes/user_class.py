916a69d172b8e3b91fd0ba3d446869f5606c193f
WilliamKulha <wkulha14@gmail.com>
2019-07-01 11:06:00 -0400

    Add an admin sublcass to the user class and then pull admin priveleges out into their own class that is used as a property of the admin class.

--- classes/user_class.py
+++ classes/user_class.py
@@ -1,3 +1,16 @@
+class Priveleges :
+  """A class to represent priveleges"""
+  def __init__(self, priveleges) :
+    """Initialize the list of priveleges"""
+    self.priveleges = priveleges
+
+  def display_priveleges(self) :
+    """ Method to list all the priveleges of the admin user."""
+    print(f"This administrator has the following priveleges:")
+    for privelege in self.priveleges :
+      print(f"{privelege}")
+
+
 class User :
   """A naive attempt to model a user object"""
   def __init__(self, first_name, last_name, age, location, profession) :
@@ -17,10 +30,16 @@ class User :
   def greet_user(self) :
     """Greet the user by their first name. """
     print(f"\nWell hey there {self.first_name}! How're you today?")
-  
 
-jeff_d = User("Jeff", "Dorowicz", 22, "Richmond", "Painter")
+class Admin(User) :
+    """A naive attempt to add an administrator subclass to the User class"""
+    def __init__(self, first_name, last_name, age, location, profession, priveleges) :
+      """Initialize attributes of the parent class"""
+      super().__init__(first_name, last_name, age, location, profession)
+      self.priveleges = Priveleges(priveleges)
+    
+admin_jake = Admin(
+    'Jake', 'Rubberneck', 33, 'Johannesburg', 'Electrician', ['Approve posts', 'Reject posts', 'Ban users'])
 
-jeff_d.print_self()
+admin_jake.priveleges.display_priveleges()
 
-jeff_d.greet_user()
